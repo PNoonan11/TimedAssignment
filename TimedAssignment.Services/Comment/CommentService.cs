4ee6dd1208e0462d10623efeb417b4e59a764018
David Sullivan <dcsulliv@gmail.com>
2022-08-13 12:54:54 -0400

    crud and controller built out redo

--- TimedAssignment.Services/Comment/CommentService.cs
+++ TimedAssignment.Services/Comment/CommentService.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TimedAssignment.Data;
 using TimedAssignment.Models.Comment;
 using TimedAssignment.Data.Entities;
-using TimedAssignment.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TimedAssignment.Services.Comment
 {
@@ -19,16 +15,89 @@ namespace TimedAssignment.Services.Comment
 
         public async Task<bool> CreateACommentAsync(CommentCreate model)
         {
+            if (await GettCommentByUsername(model.UserName) != null)
+            return false;
             var entity = new CommentEntity
             {
+                Id = model.Id,
                 Text = model.Text,
-                UserName = model.UserName
+                UserName = model.UserName,
+                
             };
             _context.Comments.Add(entity);
             var numberOfChanges = await _context.SaveChangesAsync();
 
             return numberOfChanges == 1;
             
+        }
+
+        private async Task<CommentEntity> GettCommentByUsername(string UserName)
+        {
+            return await _context.Comments.FirstOrDefaultAsync(Comment => Comment.UserName.ToLower() == UserName.ToLower());
+        }
+
+        public async Task<CommentDetail> GetCommentbyIdAsync(int commentId)
+        {
+            var entity = await _context.Comments.FindAsync(commentId);
+            if(entity is null)
+            return null;
+
+            var commentDetail = new CommentDetail
+            {
+                Id = entity.Id,
+                UserName = entity.UserName,
+                Text = entity.Text,
+                CommentId = entity.CommentId
+            };
+
+            return commentDetail;
+        }
+
+        public async Task<IEnumerable<CommentListItem>> GetAllCommentsAsync()
+        {
+            var comments = await _context.Comments
+                    .Select(entity => new CommentListItem
+                    {
+                        Id = entity.Id,
+                        Text = entity.Text,
+                        Username = entity.UserName,
+                    })
+                    .ToListAsync();
+
+                    return comments;
+
+        }
+
+        public async Task<bool> UpdateCommentAsync(CommentUpdate request)
+        {
+            var commentEntity = await _context.Comments.FindAsync(request.Id);
+
+            commentEntity.Id = request.Id;
+            commentEntity.Text = request.Text;
+            commentEntity.UserName = request.UserName;
+
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+
+        }
+
+        public async Task<bool> DeleteACommentAsync(int commentId)
+        {
+            var commentEntity = await _context.Comments.FindAsync(commentId);
+
+            _context.Comments.Remove(commentEntity);
+            return await _context.SaveChangesAsync() == 1;
+        }
+
+        public Task<CommentDetail> GetCommentByIdAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateACommentAsync(CommentUpdate request)
+        {
+            throw new NotImplementedException();
         }
     }
 }
